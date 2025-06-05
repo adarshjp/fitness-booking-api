d105dcd0d135b607c059c41d9e5733b203e4733e
adarshjp <adarsh.24padmashali@gmail.com>
2025-06-05 23:30:25 +0530

    created model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class FitnessClass(models.Model):
+    name = models.CharField(max_length=50)
+    instructor = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    total_slots = models.IntegerField()
+    available_slots = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} by {self.instructor} at {self.start_time}"
+
+class Booking(models.Model):
+    fitness_class = models.ForeignKey(FitnessClass, on_delete=models.CASCADE)
+    client_name = models.CharField(max_length=100)
+    client_email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.client_name} - {self.fitness_class.name}"
